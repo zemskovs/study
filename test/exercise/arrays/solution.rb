138bdd7df62fc5c2f7fb5ff26581b43fdcb0a65e
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-07-03 09:58:12 +0300

    add replace method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_array_number = array.min { |a, b| b <=> a }
+        replaced_array = array.map { |num| replace_negative(num, max_array_number) }
+        replaced_array
       end
 
       def search(_array, _query)
         0
+      end
+
+      private
+
+      def replace_negative(num, max_num)
+        return max_num if num.positive?
+
+        num
       end
     end
   end
