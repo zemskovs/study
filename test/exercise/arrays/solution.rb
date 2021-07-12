ae12cc2852f6a3ab5de77115f280a8f2643cbe4f
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-07-12 08:26:18 +0300

    fix formatting

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,30 +2,30 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max_array_number = array.min { |a, b| b <=> a }
-        replaced_array = array.map { |num| replace_negative(num, max_array_number) }
+        max_array_number = array.max
+        replaced_array = array.map { |num| replace_positive(num, max_array_number) }
         replaced_array
       end
 
-      def search(_array, _query)
-        return -1 if _array.empty?
+      def search(array, query)
+        return -1 if array.empty?
 
-        middle_index = _array.length / 2
-        current_value = _array[middle_index]
+        middle_index = array.length / 2
+        current_value = array[middle_index]
 
-        return middle_index if current_value == _query
+        return middle_index if current_value == query
 
-        return search(_array.take(middle_index), _query) if _query < current_value
+        return search(array.take(middle_index), query) if query < current_value
 
-        if _query > current_value
-          corrective_index = search(_array.drop(middle_index + 1), _query)
+        if query > current_value
+          corrective_index = search(array.drop(middle_index + 1), query)
           corrective_index == -1 ? -1 : (middle_index + 1) + corrective_index
         end
       end
 
       private
 
-      def replace_negative(num, max_num)
+      def replace_positive(num, max_num)
         return max_num if num.positive?
 
         num
