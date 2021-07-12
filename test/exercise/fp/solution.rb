160e9fadf534f8cee979cf734988c8ab17ad4324
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-07-12 21:41:35 +0300

    add chars_count

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,26 +6,25 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-
-      # написать сначала select, потом map, где разбить по количеству стран, потом reduce
       def rating(array)
         selection = array
-                    .map { |film| map_film(film) }
-                    .select { |film| select_film(film) }
+                    .map { |film| map_raiting_film(film) }
+                    .select { |film| select_raiting_film(film) }
 
         total_raiting = selection.reduce(0) { |acc, film| acc + film[:raiting] }
         total_count = selection.length
 
-        return total_raiting / total_count
+        total_raiting / total_count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        selection = films.select { |film| select_chars_film(film, threshold) }.map { |film| map_chars_film(film) }
+        selection.sum
       end
 
       private
 
-      def map_film(film)
+      def map_raiting_film(film)
         raiting = film['rating_kinopoisk'].to_s.to_f
         country_count = film['country'].to_s.split(',').length
 
@@ -37,13 +36,17 @@ module Exercise
         mapped_film
       end
 
-      def select_film(film)
+      def select_raiting_film(film)
         !film[:raiting].zero? && film[:country_count] >= 2
       end
 
-      def reduce_film(raiting_params, film)
-        raiting_params[:total_raiting] = raiting_params[:total_raiting] + film[:raiting].to_i,
-                                         raiting_params[:total_count] = raiting_params[:total_count] + 1
+      def select_chars_film(film, raiting)
+        film_raiting = film['rating_kinopoisk'].to_s.to_f
+        film_raiting >= raiting
+      end
+
+      def map_chars_film(film)
+        film['name'].to_s.scan('и').count
       end
     end
   end
